fd2d203e2123f589b2a452a0b66c134180ac8f2b
hvduan97 <hvduan97@gmail.com>
2020-07-14 15:28:35 +0700

    duan commit

--- service/src/main/java/com/codegym/service/BillService.java
+++ service/src/main/java/com/codegym/service/BillService.java
@@ -1,12 +1,12 @@
-package com.codegym.service;
-
-import com.codegym.dao.entity.Bill;
-
-import java.util.List;
-
-public interface BillService {
-    List<Bill> findAllByDeleteFlagFalse();
-    Bill findById(Integer id);
-    void save(Bill bill);
-    void remove(Bill bill);
-}
+//package com.codegym.service;
+//
+//import com.codegym.dao.entity.Bill;
+//
+//import java.util.List;
+//
+//public interface BillService {
+//    List<Bill> findAllByDeleteFlagFalse();
+//    Bill findById(Integer id);
+//    void save(Bill bill);
+//    void remove(Bill bill);
+//}
