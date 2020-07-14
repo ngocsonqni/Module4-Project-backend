fa954aeb5fc2607363706036e2de0b15e5f382ef
NgocThach <hnt1693@gmail.com>
2020-07-14 15:35:43 +0700

    Update backend

--- web_service/src/main/java/com/codegym/web_service/Controller/employeeController/employeeController.java
+++ web_service/src/main/java/com/codegym/web_service/Controller/employeeController/employeeController.java
@@ -1,4 +1,16 @@
 package com.codegym.web_service.Controller.employeeController;
 
+import com.codegym.dao.entity.Distributor;
+import com.codegym.dao.entity.TypeOfDistributor;
+import com.codegym.service.DistributorService;
+import com.codegym.service.TypeOfDistributorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class employeeController {
+
 }
