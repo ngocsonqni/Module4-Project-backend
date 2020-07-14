35da3fd9f3bb02eb6dca988238882d4e1fef0d2d
haidakto321 <kieusonhai07@gmail.com>
2020-07-14 15:03:56 +0700

    hai lam employee

--- service/src/main/java/com/codegym/service/EmployeeService.java
+++ service/src/main/java/com/codegym/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.codegym.service;
 
+import com.codegym.dao.entity.Employee;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
 public interface EmployeeService {
+    List<Employee> findAll();
+    List<Employee> findAllDeleted();
+    Employee findById(int id);
+    void save(Employee employee);
+    void remove(int id);
 }
--- service/src/main/java/com/codegym/service/Impl/EmployeeServiceImpl.java
+++ service/src/main/java/com/codegym/service/Impl/EmployeeServiceImpl.java
@@ -1,4 +1,42 @@
 package com.codegym.service.Impl;
 
-public class EmployeeServiceImpl {
+import com.codegym.dao.entity.Employee;
+import com.codegym.dao.repository.EmployeeRepository;
+import com.codegym.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAllByDeleteFlagIsFalse();
+    }
+
+    @Override
+    public List<Employee> findAllDeleted() {
+        return employeeRepository.findAllByDeleteFlagIsTrue();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeRepository.findByDeleteFlagIsFalseAndId(id);
+    }
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void remove(int id) {
+        Employee employee = employeeRepository.findByDeleteFlagIsFalseAndId(id);
+        employee.setDeleteFlag(true);
+    }
 }
