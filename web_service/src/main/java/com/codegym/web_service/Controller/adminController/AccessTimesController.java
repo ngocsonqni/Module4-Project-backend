89d5aec49899926905763e8512e479b706b70c9f
HoKhanhNguyen99 <honvong13@gmail.com>
2020-07-17 09:09:28 +0700

    update admin

---
+++ web_service/src/main/java/com/codegym/web_service/Controller/adminController/AccessTimesController.java
@@ -0,0 +1,41 @@
+package com.codegym.web_service.Controller.adminController;
+
+import com.codegym.dao.entity.AccessTimesDTO;
+import com.codegym.dao.repository.AccessTimesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+public class AccessTimesController {
+    @Autowired
+    AccessTimesRepository accessTimesRepository;
+
+    @RequestMapping(value = "/access-times", method = RequestMethod.GET)
+    public ResponseEntity<List<AccessTimesDTO>> avc() {
+        List<AccessTimesDTO> localDateTimeIntegerMap = accessTimesRepository.countAccessTimes();
+        for(int i =0;i<localDateTimeIntegerMap.size();i++){
+            if(localDateTimeIntegerMap.size()>12){
+                localDateTimeIntegerMap.remove(0);
+            }else {
+                break;
+            }
+        }
+        if (localDateTimeIntegerMap.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(localDateTimeIntegerMap, HttpStatus.OK);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
