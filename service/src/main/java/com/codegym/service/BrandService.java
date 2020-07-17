54b3bfc6f81b601c2dc7b68d82af27829e492eb1
ngocsonqni <sonnn.qni@gmail.com>
2020-07-17 16:48:49 +0700

    add file

--- service/src/main/java/com/codegym/service/BrandService.java
+++ service/src/main/java/com/codegym/service/BrandService.java
@@ -3,19 +3,13 @@ package com.codegym.service;
 import com.codegym.dao.entity.Brand;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.JpaRepository;
-=======
->>>>>>> 5fcf850802259f604d5893956cd1e663f9566198
 
 import java.util.List;
 
 public interface BrandService {
     List<Brand> getAllBrand();
     boolean createBrand(Brand brand);
-<<<<<<< HEAD
-    Page<Brand> findAllByNameContaining(Pageable pageable, String search);
-=======
     Page<Brand> findAllByBrandNameContaining(Pageable pageable, String search);
->>>>>>> 5fcf850802259f604d5893956cd1e663f9566198
+
 }
