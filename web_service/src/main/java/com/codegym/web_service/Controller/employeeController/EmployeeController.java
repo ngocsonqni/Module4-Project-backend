20c450a7755a759903f8c9fe76978c2430a9e9ce
haidakto321 <kieusonhai07@gmail.com>
2020-08-07 13:44:26 +0700

    hai

--- web_service/src/main/java/com/codegym/web_service/Controller/employeeController/EmployeeController.java
+++ web_service/src/main/java/com/codegym/web_service/Controller/employeeController/EmployeeController.java
@@ -135,8 +135,7 @@ public class EmployeeController {
     //handle error
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
