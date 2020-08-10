475147c92d0d7b76577718e4b6d253109ed000bb
C0220G1-LeMinhHuy <lehuy121@gmail.com>
2020-08-10 21:34:48 +0700

    push code search + create

--- dao/src/main/java/com/codegym/dao/repository/ProductRepository.java
+++ dao/src/main/java/com/codegym/dao/repository/ProductRepository.java
@@ -13,14 +13,63 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     Page<Product> findAllByDeleteFlagFalse(Pageable pageable);
+
     /**
      * @return all data from Product with categoryId is false
      */
     List<Product> findDistinctByCategory_CategoryIdAndDeleteFlagIsFalseOrderByBrand(Integer categoryId);
 
     Page<Product> findAllByCategory_CategoryIdAndBrand_IdAndDeleteFlagIsFalse(Integer categoryId, Integer BrandId, Pageable pageable);
+
+    /**
+     * search with categoryId, brandID, productName, price
+     */
+    Page<Product> findAllByCategory_CategoryIdAndBrand_IdAndProductNameContainingAndPriceContainingAndDeleteFlagIsFalseAndDeleteFlagIsFalse(Integer categoryId, Integer brandId, String productName, String price, Pageable pageable);
+
+    /**
+     * search with categoryId, brandID, productName
+     */
+    Page<Product> findAllByCategory_CategoryIdAndBrand_IdAndProductNameContainingAndDeleteFlagIsFalseAndDeleteFlagIsFalse(Integer categoryId, Integer brandId, String productName, Pageable pageable);
+
+    /**
+     * search with categoryId, brandID, price
+     */
+    Page<Product> findAllByCategory_CategoryIdAndBrand_IdAndPriceContainingAndDeleteFlagIsFalse(Integer categoryId, Integer brandId, String price, Pageable pageable);
+
+    /**
+     * search with categoryId, productName, price
+     */
+    Page<Product> findAllByCategory_CategoryIdAndProductNameContainingAndPriceContainingAndDeleteFlagIsFalse(Integer categoryId, String productName, String price, Pageable pageable);
+
+    /**
+     * search with categoryId, productName
+     */
+    Page<Product> findAllByCategory_CategoryIdAndProductNameContainingAndDeleteFlagIsFalse(Integer categoryId, String productName, Pageable pageable);
+
+    /**
+     * search with categoryId, price
+     */
+    Page<Product> findAllByCategory_CategoryIdAndPriceContainingAndDeleteFlagIsFalse(Integer categoryId, String price, Pageable pageable);
+
+    /**
+     * search with productName, price
+     */
+    Page<Product> findAllByProductNameContainingAndPriceContainingAndDeleteFlagIsFalse(String productName, String price, Pageable pageable);
+
+    /**
+     * search with productName
+     */
+    Page<Product> findAllByProductNameContainingAndDeleteFlagIsFalse(String productName, Pageable pageable);
+
+    /**
+     * search with price
+     */
+    Page<Product> findAllByPriceContainingAndDeleteFlagIsFalse(String price, Pageable pageable);
+
     Page<Product> findAllByCategory_CategoryIdAndDeleteFlagIsFalse(Integer categoryId, Pageable pageable);
-   Product findByProductIdAndDeleteFlagFalse(Integer id);
+
+    Product findByProductIdAndDeleteFlagFalse(Integer id);
+
     List<Product> findAllByCategory_CategoryId(Integer categoryId);
 
     @Query(nativeQuery = true, value = "SELECT * " +
