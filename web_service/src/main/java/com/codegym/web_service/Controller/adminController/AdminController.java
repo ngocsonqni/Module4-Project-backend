31cedf11402f407d0a650475f62802c21bfe6e84
Tran Chi Hieu <hieu57130154@gmail.com>
2020-08-18 09:44:04 +0700

    hieu project ok

--- web_service/src/main/java/com/codegym/web_service/Controller/adminController/AdminController.java
+++ web_service/src/main/java/com/codegym/web_service/Controller/adminController/AdminController.java
@@ -155,15 +155,15 @@ public class AdminController {
             currentAccount.setRole(account.getRole());
             currentAccount.setDeleteFlag(account.getDeleteFlag());
             accountService.save(currentAccount);
+            if (account.getRole().getRoleName().equals("ROLE_ADMIN") || account.getRole().getRoleName().equals("ROLE_PARTNER") || account.getRole().getRoleName().equals("ROLE_WAREHOUSE")) {
+                asyncDeleteAccount.sendEmailWithEmployee(employeeService.findByAccountId(account.getAccountId()));
+            } else {
+                asyncDeleteAccount.sendEmailWithUser(userService.findUserByAccountId(account.getAccountId()));
+            }
+            return new ResponseEntity<Account>(currentAccount, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<Account>(HttpStatus.NOT_ACCEPTABLE);
         }
-        if (account.getRole().getRoleName().equals("ROLE_ADMIN") || account.getRole().getRoleName().equals("ROLE_PARTNER") || account.getRole().getRoleName().equals("ROLE_WAREHOUSE")) {
-            asyncDeleteAccount.sendEmailWithEmployee(employeeService.findByAccountId(account.getAccountId()));
-        } else {
-            asyncDeleteAccount.sendEmailWithUser(userService.findUserByAccountId(account.getAccountId()));
-        }
-        return new ResponseEntity<Account>(currentAccount, HttpStatus.OK);
     }
 
     //--------------------------------- details Employee ---------------------------
